dea8564ecfd6fdb5f0496a3759e4a4a6b536b1f4
Meriadec LANTONNET <mlantonn@student.42.fr>
2019-07-18 18:12:52 +0200

    remade col formation

--- srcs/print_formatted.c
+++ srcs/print_formatted.c
@@ -6,101 +6,38 @@
 /*   By: mlantonn <mlantonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 12:04:19 by mlantonn          #+#    #+#             */
-/*   Updated: 2019/06/12 19:19:29 by mlantonn         ###   ########.fr       */
+/*   Updated: 2019/07/18 18:11:40 by mlantonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-static int	get_lines_len(t_env *env, int lines)
+static int	get_width(t_env *env)
 {
-	int	cols;
-	int	tmp;
-	int	len;
 	int	i;
-	int	j;
+	int	max;
+	int	tmp;
 
 	i = -1;
-	len = 0;
-	cols = (env->size / lines) + 1;
-	while (++i < lines)
+	max = 1;
+	while (++i < env->size)
 	{
-		j = -1;
 		tmp = 0;
-		while (++j < cols)
-		{
-			if (((j * lines) + i < env->size) && (!env->opt['P']
-					|| (env->contents[(j * lines) + i].type != 'd'
-						&& env->contents[(j * lines) + i].type != 'l')))
-				tmp += env->contents[(j * lines) + i].name_len + 2;
-		}
-		len = len >= tmp ? len : tmp;
+		while (env->contents[i].name[tmp])
+			++tmp;
+		if (tmp > max)
+			max = tmp;
 	}
-	return (len == 0 ? -1 : len);
+	return (((max / 8) * 8) + 8);
 }
 
-static void	set_name_width(t_env *env, int lines)
-{
-	int cols;
-	int tmp;
-	int i;
-	int j;
-	int k;
-
-	cols = (env->size / lines) + 1;
-	i = -1;
-	while (++i < cols)
-		if (i < env->size)
-			env->contents[i].name_width = 0;
-	i = -1;
-	while (++i < lines)
-	{
-		j = -1;
-		while (++j < cols)
-		{
-			k = (j * lines) + i;
-			tmp = k < env->size ? env->contents[k].name_len + 2 : 0;
-			if (j < env->size)
-				env->contents[j].name_width = env->contents[j].name_width \
-					>= tmp ? env->contents[j].name_width : tmp;
-		}
-	}
-}
-
-static int	get_lines(t_env *env)
-{
-	t_winsize	ws;
-	int			lines;
-	int			len;
-
-	if (ioctl(1, TIOCGWINSZ, &ws))
-		return (0);
-	lines = 1;
-	while (lines < ((env->size) / 2) + 1)
-	{
-		len = get_lines_len(env, lines);
-		if (len < 0)
-			return (0);
-		if (len < ws.ws_col)
-		{
-			set_name_width(env, lines);
-			return (lines);
-		}
-		++lines;
-	}
-	return (0);
-}
-
-int			print_formatted(t_env *env)
+static void		print(t_env *env, int cols, int width)
 {
 	int lines;
-	int cols;
 	int i;
 	int j;
 
-	if (!(lines = get_lines(env)))
-		return (1);
-	cols = (env->size / lines) + 1;
+	lines = env->size / cols + (env->size % cols != 0);
 	i = -1;
 	while (++i < lines)
 	{
@@ -111,11 +48,26 @@ int			print_formatted(t_env *env)
 					&& (env->contents[(j * lines) + i].type == 'd'
 						|| env->contents[(j * lines) + i].type == 'l')))
 				continue ;
-			ft_printf_static("%-*s", j < env->size ? env->contents[j].name_width : 0,
-				env->contents[(j * lines) + i].name);
+			ft_printf_static("%-*s", width, env->contents[(j * lines) + i].name);
 			env->printed++;
 		}
-		ft_printf_static("\n");
+		if (env->printed)
+			ft_printf_static("\n");
 	}
+}
+
+int			print_formatted(t_env *env)
+{
+	t_winsize	ws;
+	int			width;
+	int			cols;
+
+	if ((ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws)))
+		return (1);
+	width = get_width(env);
+	cols = (((ws.ws_col * 8) / 8) + 8) / width;
+	if (cols == 1)
+		return (1);
+	print(env, cols, width);
 	return (0);
 }
